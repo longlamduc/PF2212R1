abfbe5b5f88900bc8af3d80e520bcc1187f3fe55
Long <longlamduc@gmail.com>
2023-03-03 20:14:24 +0700

    Lesson 18 - Demo Function

---
+++ Buổi 18 - 01-03-2023/demo_function.js
@@ -0,0 +1,28 @@
+// Nhập vào 2 số a, b. Tính tổng của hai số vừa nhập.
+// Nhập vào 2 số c, d. Tính tổng của hai số vừa nhập.
+
+// let a = parseInt(prompt("Please input a: "));
+// let b = parseInt(prompt("Please input b: "));
+
+// let sumAB = a + b;
+
+// console.log(sumAB);
+
+// let c = parseInt(prompt("Please input c: "));
+// let d = parseInt(prompt("Please input d: "));
+
+// let sumCD = c + d;
+// console.log(sumCD);
+
+function calculateSum() {
+    let n1 = parseInt(prompt("Please input n1: "));
+    let n2 = parseInt(prompt("Please input n2: "));
+
+    let total = n1 + n2;
+
+    console.log(total);
+}
+
+calculateSum();
+calculateSum();
+calculateSum();
---
+++ Buổi 18 - 01-03-2023/practice-1.js
@@ -0,0 +1,11 @@
+// Viết hàm sayHello(), hàm này khi được gọi sẽ hiển thị
+// alert với nội dung "HelloWorld!"
+
+function sayHello() {
+    alert("HelloWorld!");
+}
+
+sayHello();
+sayHello();
+sayHello();
+sayHello();
